b66973d9926ba8f74b3c914b94aa50a157f7a1b4
Dgraber27 <drewg2727@gmail.com>
2020-09-02 14:41:43 -0400

    UserController

---
+++ SocialMediaAssignment/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNet.Identity;
+using SocialMedia.Models;
+using SocialMedia.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace SocialMediaAssignment.Controllers
+{
+   
+        [Authorize]
+        public class UserController : ApiController
+        {
+            private UserService CreateUserService()
+            {
+                var userId = Guid.Parse(User.Identity.GetUserId());
+                var userEmail = User.Identity.GetUserName();
+                var UserService = new UserService(userId, userEmail);
+                return UserService;
+            }
+
+            [HttpPost]
+            public IHttpActionResult CreateUser(UserCreate user)
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                var service = CreateUserService();
+
+                if (!service.CreateUser(user))
+                    return InternalServerError();
+
+                return Ok("Account successfully created.");
+            }
+
+        }
+    
+}
