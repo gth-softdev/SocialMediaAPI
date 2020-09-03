10ee6ab8d9998c5395d664d6d5f4d007a800b881
gth-softdev <68547258+gth-softdev@users.noreply.github.com>
2020-09-02 21:58:44 -0400

    posts work

--- SocialMedia.Models/PostCreate.cs
+++ SocialMedia.Models/PostCreate.cs
@@ -9,6 +9,8 @@ namespace SocialMedia.Models
 {
     public class PostCreate
     {
+        public int PostId { get; set; }
+
         [Required]
         [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
         [MaxLength(100, ErrorMessage = "There are too many characters in this field.")]
